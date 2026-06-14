7f1428ec6c9a6a93a6d11ef96821ce2da904864a
israel balbino <israel-balbino2012@hotmail.com>
2026-06-14 05:59:33 -0300

    backendtopwhats

---
+++ server.js
@@ -0,0 +1,70 @@
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+async function sendWhatsApp(message) {
+  try {
+   const response =  await axios.post(
+      `https://api.w-api.app/v1/message/send-text?instanceId=${process.env.W_API_INSTANCE}`,
+      {
+        phone: process.env.MY_NUMBER,
+        message
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.WAPI_TOKEN}`,
+          "Content-Type": "application/json"
+        }
+      }
+    );
+
+    console.log("Mensagem enviada:",response.data);
+  } catch (error) {
+    console.log(
+      "Erro ao enviar:",
+      error.response?.data || error.message
+    );
+  }
+}
+
+app.post("/webhook/perfectpay", async (req, res) => {
+  try {
+    const data = req.body;
+
+   // console.log("Webhook recebido:", data);
+
+    if (data?.sale_status_enum_key === "approved") {
+      const customer = data.customer.full_name;
+      const product = data.product.name;
+      const value = data.sale_amount;
+
+      console.log(customer)
+
+      const message = `
+🔥 NOVA VENDA - Você é TOP  
+
+👤 Cliente: ${customer}
+💰 Valor: R$ ${value}
+
+      `;
+
+      //console.log(data)
+
+      await sendWhatsApp(message);
+    }
+
+    return res.status(200).json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "erro interno" });
+  }
+});
+
+app.listen(process.env.PORT || 3333, () => {
+  console.log("Servidor rodando");
+});
